5f0c758be876609d2c117c580874ce400c032b02
Sumaiya Aziz <163577070+SumaiyaAz2@users.noreply.github.com>
2024-05-13 01:41:48 +0600

    Transfer SQlite3 to PG

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_02_192550) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "patients", force: :cascade do |t|
     t.string "name", null: false
     t.string "contact", null: false
     t.string "address", null: false
     t.string "nid", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_patients_on_user_id"
@@ -33,8 +36,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_192550) do
   create_table "records", force: :cascade do |t|
     t.string "disease"
     t.string "medicine"
-    t.integer "patient_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "patient_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nid"
